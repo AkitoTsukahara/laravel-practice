4a47a8cc70270e0ac543a56f172e74cf3d11bdfe
AkitoTsukahara <tsukahara.akito@gmail.com>
2020-11-13 23:32:54 +0900

    [add]REST route

--- server/routes/web.php
+++ server/routes/web.php
@@ -18,6 +18,13 @@ Route::get('/', function () {
 
 Route::get('/tests/test', 'TestController@index');
 
+Route::group(['prefix' => 'contact', 'middleware' => 'auth'], function() {
+    Route::get('index', 'ContactFormController@index')->name('contact.index');
+});
+
+//REST
+//Route::resource('contacts', 'ContactFormController');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
